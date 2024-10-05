d1d905a7f9c61cf1649b91da8300656b49a48a99
Yassir Yassin <yassirscreed@hotmail.com>
2024-10-05 17:10:03 +0100

    Added topojson and initial choropleth

--- idioms/choropleth_map.js
+++ idioms/choropleth_map.js
@@ -1,152 +1,122 @@
-// Function to create the choropleth map
 function createChoroplethMap(data, containerId) {
-    // Filter the data to remove entries with missing incomeperperson values
-    currentData = globalDataCapita.filter(function (d) {
-      return d.incomeperperson != "";
-    });
+    // Set up dimensions
+    const margin = { top: 20, right: 20, bottom: 50, left: 80 };
+    const width = 960 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
   
-  
-    // Create a title for the choropleth map
-    const chartTitle = d3
-      .select("#choroplethTitle")
-      .append("text")
-      .attr("x", width / 2)
-      .attr("y", margin.top)
-      .text("Income per person");
-  
-  
-    // Create an SVG element to hold the map
-    const svg = d3
-      .select("#choropleth")
+    // Create SVG
+    const svg = d3.select(containerId)
       .append("svg")
-      .attr("width", width)
-      .attr("height", height);
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
   
+    // Create a color scale
+    const colorScale = d3.scaleQuantile()
+      .domain(d3.extent(data, d => +d.happiness_score))
+      .range(d3.schemeBlues[9]);
   
-    // Create a group to hold the map elements
-    const mapGroup = svg.append("g");
+    // Load world map data
+    d3.json("./data/data.json").then(function(worldData) {
+      const countries = topojson.feature(worldData, worldData.objects.countries);
   
+      // Create a map projection
+      const projection = d3.geoMercator()
+        .fitSize([width, height], countries);
   
-    // Create a color scale for the incomeperperson values
-    const colorScale = d3
-      .scaleLog()
-      .domain([
-        d3.min(currentData, (d) => d.incomeperperson),
-        d3.max(currentData, (d) => d.incomeperperson),
-      ])
-      .range([0, 1]);
+      // Create a path generator
+      const path = d3.geoPath().projection(projection);
   
-  
-    // Create a projection to convert geo-coordinates to pixel values
-    const projection = d3
-      .geoMercator()
-      .fitSize([width, height], globalDataCountries);
-  
-  
-    // Create a path generator for the map
-    const path = d3.geoPath().projection(projection);
-  
-  
-    // Add countries as path elements to the map
-    mapGroup
-      .selectAll(".country")
-      .data(globalDataCountries.features)
-      .enter()
-      .append("path")
-      .attr("class", "country data")
-      .attr("d", path)
-      .attr("stroke", "black")
-      .on("mouseover", handleMouseOver) // Function to handle mouseover event
-      .on("mouseout", handleMouseOut)   // Function to handle mouseout event
-      .append("title")
-      .text((d) => d.properties.name);
-  
-  
-    // Set the fill color of each country based on its incomeperperson value
-    currentData.forEach((element) => {
-      mapGroup
-        .selectAll("path")
-        .filter(function (d) {
-          return d.properties.name == element.country;
+      // Draw the map
+      svg.selectAll("path")
+        .data(countries.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("fill", d => {
+          const countryData = data.find(item => item.country === d.properties.name);
+          return countryData ? colorScale(+countryData.happiness_score) : "#ccc";
         })
-        .attr("fill", d3.interpolateBlues(colorScale(element.incomeperperson)));
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 0.5)
+        .on("mouseover", function(event, d) {
+          const countryData = data.find(item => item.country === d.properties.name);
+          if (countryData) {
+            d3.select(this).attr("stroke", "#000").attr("stroke-width", 2);
+            showTooltip(event, d, countryData);
+          }
+        })
+        .on("mouseout", function() {
+          d3.select(this).attr("stroke", "#fff").attr("stroke-width", 0.5);
+          hideTooltip();
+        });
+  
+      // Add zoom functionality
+      const zoom = d3.zoom()
+        .scaleExtent([1, 8])
+        .on("zoom", zoomed);
+  
+      svg.call(zoom);
+  
+      function zoomed(event) {
+        svg.selectAll("path")
+          .attr("transform", event.transform);
+      }
+  
+      // Add legend
+      const legend = svg.append("g")
+        .attr("transform", `translate(${width - 120}, ${height - 180})`);
+  
+      const legendScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.happiness_score))
+        .range([0, 180]);
+  
+      const legendAxis = d3.axisRight(legendScale)
+        .ticks(5)
+        .tickSize(20);
+  
+      legend.selectAll("rect")
+        .data(colorScale.range())
+        .enter()
+        .append("rect")
+        .attr("y", (d, i) => i * 20)
+        .attr("width", 20)
+        .attr("height", 20)
+        .style("fill", d => d);
+  
+      legend.call(legendAxis);
+  
+      legend.append("text")
+        .attr("x", 10)
+        .attr("y", -10)
+        .attr("text-anchor", "middle")
+        .text("Happiness Score");
     });
   
+    // Tooltip functions
+    function showTooltip(event, d, countryData) {
+      const tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("position", "absolute")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-radius", "5px")
+        .style("padding", "10px");
   
-    // Create zoom behavior for the map
-    const zoom = d3
-      .zoom()
-      .scaleExtent([1, 8])
-      .translateExtent([
-        [0, 0],
-        [width, height],
-      ])
-      .on("zoom", zoomed);
-  
-  
-    // Apply zoom behavior to the SVG element
-    svg.call(zoom);
-  
-  
-    // Function to handle the zoom event
-    function zoomed(event) {
-      mapGroup.attr("transform", event.transform);
+      tooltip.html(`
+        <strong>${d.properties.name}</strong><br>
+        Happiness Score: ${countryData.happiness_score}<br>
+        GDP per capita: ${countryData.gdp_per_capita}<br>
+        Social support: ${countryData.social_support}<br>
+        Healthy life expectancy: ${countryData.healthy_life_expectancy}
+      `)
+        .style("left", (event.pageX + 10) + "px")
+        .style("top", (event.pageY - 28) + "px");
     }
   
-  
-    // Create a legend for the choropleth map
-    const svg2 = d3
-      .select("#choroplethLabel")
-      .append("svg")
-      .attr("width", width * 0.2)
-      .attr("height", height);
-  
-  
-    // Create a gradient for the legend color scale
-    const defs = svg2.append("defs");
-    const gradient = defs
-      .append("linearGradient")
-      .attr("id", "colorScaleGradient")
-      .attr("x1", "0%")
-      .attr("y1", "0%")
-      .attr("x2", "0%")
-      .attr("y2", "100%");
-  
-  
-    gradient
-      .append("stop")
-      .attr("offset", "0%")
-      .attr("stop-color", d3.interpolateBlues(0));
-  
-  
-    gradient
-      .append("stop")
-      .attr("offset", "100%")
-      .attr("stop-color", d3.interpolateBlues(1));
-  
-  
-    // Create the legend rectangle filled with the color scale gradient
-    const legend = svg2.append("g").attr("transform", `translate(0, 40)`);
-    const legendHeight = height - 40;
-    const legendWidth = 20;
-  
-  
-    legend
-      .append("rect")
-      .attr("width", legendWidth)
-      .attr("height", legendHeight)
-      .style("fill", "url(#colorScaleGradient)");
-  
-  
-    // Add tick marks and labels to the legend
-    for (let index = 0; index <= 1; index += 0.25) {
-      legend
-        .append("text")
-        .attr("x", legendWidth + 5)
-        .attr("y", legendHeight * index)
-        .text(Math.round(colorScale.invert(index)));
+    function hideTooltip() {
+      d3.select(".tooltip").remove();
     }
   }
-  
-  
-  
